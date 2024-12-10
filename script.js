fee9a5a7fdc8b33b89628f74e161b48d3d9295e9
tatianabojk <t.bojko@urent.city>
2024-12-10 20:55:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slides img');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-    const slideWidth = document.querySelector('.slider').offsetWidth;
-    document.querySelector('.slides').style.transform = `translateX(-${index * slideWidth}px)`;
-}
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    showSlide(currentIndex);
-}
-
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    showSlide(currentIndex);
-}
-
-// Инициализация
-showSlide(currentIndex);
-
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
