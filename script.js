8e9ec4a8fb28c6c93499783996f95d040289048f
tatianabojk <t.bojko@urent.city>
2024-12-10 22:24:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let currentSlide = 0;
-        const slides = document.querySelectorAll('.slide');
-        const nextButton = document.getElementById('nextButton');
+const slides = document.querySelectorAll('.slide');
+const nextButton = document.getElementById('nextButton');
 
-        nextButton.addEventListener('click', () => {
-            slides[currentSlide].classList.remove('active');
-            currentSlide = (currentSlide + 1) % slides.length;
-            slides[currentSlide].classList.add('active');
-        });
+nextButton.addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
